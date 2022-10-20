97ce59e3637ff18211f325dab6ceec04959ee8dc
Jeffrey Profile <jeffrey_profile@icloud.com>
2022-10-19 20:03:56 -0400

    Format changes for ScenarioStatistics.js

--- grid-dashboard/src/pages/ScenarioStatistics.js
+++ grid-dashboard/src/pages/ScenarioStatistics.js
@@ -1,98 +1,127 @@
 import AsyncSelect from "react-select/async";
-import "./ss_styles.css"
+import "./ss_styles.css";
 
-export default function ScenarioStatistics(){
-    const options = [
-       {value: "temp", label: "Temp"},
-       {value: "temp1", label: "Temp1"},
-    ];
+export default function ScenarioStatistics() {
+  const options = [
+    { value: "temp", label: "Temp" },
+    { value: "temp1", label: "Temp1" },
+  ];
 
-    const loadOptions = (searchValue, callback) => {
-      setTimeout(()=> {
-        const filterOptions = options.filter(option => 
-            option.label.toLowerCase().includes(searchValue.toLowerCase()));
-            callback(filterOptions);
-      },2000)  
-    };
+  const loadOptions = (searchValue, callback) => {
+    setTimeout(() => {
+      const filterOptions = options.filter((option) =>
+        option.label.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      callback(filterOptions);
+    }, 2000);
+  };
 
-    const showTable = () => {
-        document.getElementById("show-buttons").style.display="block";
-    };
-    return (
-        <>
-            <h1 className="scenariostats">Scenario Statistics</h1>
-            <hr></hr>
-            <h1 className="config1">Configuration</h1>
-            <hr></hr>
-             <table className="table_row" id="show-buttons">
+  const showTable = () => {
+    document.getElementById("show-buttons").style.display = "block";
+  };
+  return (
+    <>
+      <h1 className="scenariostats">Scenario Statistics</h1>
+      <hr></hr>
+      <h1 className="config1">Configuration</h1>
+      <hr></hr>
+      <table className="table_row" id="show-buttons">
         <tr>
-          <th>Metric</th> 
+          <th>Metric</th>
           <th>Value</th>
         </tr>
         <tr>
-          <td>Mean</td> 
-          <td>0</td>
-        </tr>
-        <tr >
-        <td>Median</td> 
+          <td className="first">Mean</td>
           <td>0</td>
         </tr>
         <tr>
-        <td>Mode</td> 
+          <td className="first">Median</td>
           <td>0</td>
         </tr>
         <tr>
-        <td>Std Dev</td> 
+          <td className="first">Mode</td>
           <td>0</td>
         </tr>
         <tr>
-        <td>Min</td> 
+          <td className="first">Std Dev</td>
           <td>0</td>
         </tr>
         <tr>
-        <td>Max</td> 
+          <td className="first">Min</td>
           <td>0</td>
         </tr>
         <tr>
-        <td>P99</td>
-          
-          <td>10</td>
+          <td className="first">Max</td>
+          <td>0</td>
         </tr>
         <tr>
-        <td>P95</td>
-          
-          <td>10</td>
+          <td className="first">P99</td>
+
+          <td>0</td>
         </tr>
         <tr>
-        <td>P90</td>
-          
-          <td>10</td>
+          <td className="first">P95</td>
+
+          <td>0</td>
         </tr>
         <tr>
-        <td>SKEWNESS</td>
-          
-          <td>10</td>
+          <td className="first">P90</td>
+
+          <td>0</td>
+        </tr>
+        <tr>
+          <td className="first">Skew</td>
+          <td>0</td>
         </tr>
       </table>
-                <ul className="group1">
-                    <li>Scenario</li>
-                    <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
-                </ul>
-                <ul className="group2">
-                    <li>Target Metric</li>
-                    <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
-                </ul>
-                {/* Put table div here */}
-                <ul className="group1">
-                    <li>Filter</li>
-                    <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
-                </ul>
-                <ul className="group2">
-                    <li>Aggregates</li>
-                    <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
-                </ul>
-                <button className="button1" onClick={showTable} >View Stats</button> 
-        </>
-    );
-
+      <ul className="group1">
+        <li>Scenario</li>
+        <li>
+          <AsyncSelect
+            loadOptions={loadOptions}
+            defaultOptions
+            placeholder="- Select -"
+            isClearable
+          />
+        </li>
+      </ul>
+      <ul className="group2">
+        <li>Target Metric</li>
+        <li>
+          <AsyncSelect
+            loadOptions={loadOptions}
+            defaultOptions
+            placeholder="- Select -"
+            isClearable
+          />
+        </li>
+      </ul>
+      {/* Put table div here */}
+      <ul className="group1">
+        <li>Filter</li>
+        <li>
+          <AsyncSelect
+            loadOptions={loadOptions}
+            defaultOptions
+            placeholder="- Select -"
+            isClearable
+          />
+        </li>
+      </ul>
+      <ul className="group2">
+        <li>Aggregates</li>
+        <li>
+          <AsyncSelect
+            loadOptions={loadOptions}
+            defaultOptions
+            placeholder="- Select -"
+            isClearable
+          />
+        </li>
+      </ul>
+      <button className="button1" onClick={showTable}>
+        View Stats
+      </button>
+    </>
+  );
 }
