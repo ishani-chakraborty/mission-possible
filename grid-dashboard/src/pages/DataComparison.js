8848afc1e27b5ca6f77c9feb6ad45fb69bfd1bb5
kartik-soni1707 <its.kartiksoni@gmail.com>
2022-11-02 23:35:36 -0400

    DatePicker

--- grid-dashboard/src/pages/DataComparison.js
+++ grid-dashboard/src/pages/DataComparison.js
@@ -1,11 +1,18 @@
 import AsyncSelect from "react-select/async";
 import "./dc_styles.css"
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import TimePicker from 'react-time-picker';
+import React,{useState} from 'react';
 export default function DataComparison(){
     const options = [
        {value: "temp", label: "Temp"},
        {value: "temp1", label: "Temp1"},
     ];
+    const [selectedDate, setSelectedDate]=useState(null);
+    const [selectedDate1, setSelectedDate1]=useState(null);
+  const [value, onChange] = useState('10:00');
+
 
     const loadOptions = (searchValue, callback) => {
       setTimeout(()=> {
@@ -22,13 +29,33 @@ export default function DataComparison(){
             <hr></hr>
             <ul className="headers">
                 <li>Date</li>
-                <li>Time</li>
+                <li>From</li>
+                <li>To</li>
                 <li>Scenario 1</li>
                 <li>Scenario 2</li>
             </ul>
             <ul className="dropdowns">
                 <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
-                <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
+                <li>
+                <div>
+                <DatePicker 
+                selected={selectedDate} 
+                onChange={(date)=>{setSelectedDate(date);console.log(date);}}
+                ></DatePicker>
+                <TimePicker onChange={onChange} value={value} />
+                </div>
+                
+                        </li>
+                        <li>
+                <div>
+                <DatePicker 
+                selected={selectedDate1} 
+                onChange={(date)=>{setSelectedDate1(date);console.log(date);}}
+                ></DatePicker>
+                <TimePicker onChange={onChange} value={value} />
+                </div>
+        
+                </li>
                 <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
                 <li><AsyncSelect loadOptions={loadOptions} defaultOptions placeholder="- Select -" isClearable/></li>
             </ul>
