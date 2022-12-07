53c18c7fea15dc387b2e1e2302f206f96abc9df9
Jeffrey Profile <jeffrey_profile@icloud.com>
2022-12-07 14:37:15 -0500

    Updated API

--- grid-dashboard/routes/api.js
+++ grid-dashboard/routes/api.js
@@ -16,9 +16,7 @@ const pool = new Pool(credentials);
 
 router.get("/:id", (req, res) => {
 	pool.query(`SELECT * from "${req.params.id}"`)
-		.then((result) => {
-			res.send(result.rows);
-		})
+		.then((result) => res.send(result.rows))
 		.catch((err) => res.send(err.message));
 });
 
@@ -57,12 +55,55 @@ router.get("/:id/:scenario/:pnode_name", (req, res) => {
 	}
 });
 
-router.get("/:id/:pnode_name", (req, res) => {
-	pool.query(
-		`SELECT * from "${req.params.id}" WHERE "PNODE_NAME"=${req.params.pnode_name}`
-	)
-		.then((result) => res.send(result.rows))
-		.catch((err) => res.send(err.message));
+router.get("/:id/:scenario/:pnode_name/:metric", (req, res) => {
+	if (req.params.scenario === "0") {
+		pool.query(
+			`SELECT "${req.params.metric}" from "${req.params.id}" 
+		 WHERE "PNODE_NAME"=${req.params.pnode_name}`
+		)
+			.then((result) => res.send(result.rows))
+			.catch((err) => res.send(err.message));
+	} else if (req.params.pnode_name === "0") {
+		pool.query(
+			`SELECT "${req.params.metric}" from "${req.params.id}" 
+		 WHERE "SCENARIO_ID"=${req.params.scenario}`
+		)
+			.then((result) => res.send(result.rows))
+			.catch((err) => res.send(err.message));
+	} else {
+		pool.query(
+			`SELECT "${req.params.metric}" from "${req.params.id}"
+		 WHERE "SCENARIO_ID"=${req.params.scenario} 
+		 AND "PNODE_NAME"=${req.params.pnode_name}`
+		)
+			.then((result) => res.send(result.rows))
+			.catch((err) => res.send(err.message));
+	}
+});
+
+router.get("/:id/:scenario/:pnode_name/:startdate/:enddate", (req, res) => {
+	if (req.params.scenario === 0) {
+		pool.query(
+			`SELECT * from "${req.params.id}" 
+		WHERE "PNODE_NAME"=${req.params.pnode_name}
+		AND "PERIOD_ID"
+		BETWEEN '${req.params.startdate}'   
+		AND '${req.params.enddate}'`
+		)
+			.then((result) => res.send(result.rows))
+			.catch((err) => res.send(err.message));
+	} else {
+		pool.query(
+			`SELECT * from "${req.params.id}"
+		 WHERE "SCENARIO_ID"=${req.params.scenario} 
+		 AND "PNODE_NAME"=${req.params.pnode_name}
+		 AND "PERIOD_ID"
+		 BETWEEN '${req.params.startdate}'   
+		 AND '${req.params.enddate}'`
+		)
+			.then((result) => res.send(result.rows))
+			.catch((err) => res.send(err.message));
+	}
 });
 
 router.get(
