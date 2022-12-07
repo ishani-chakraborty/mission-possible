87c821d8a6b500f2909b345f89260058f9f586f4
Jeffrey Profile <jeffrey_profile@icloud.com>
2022-12-07 14:23:42 -0500

    Populated dropdowns

--- grid-dashboard/server.js
+++ grid-dashboard/server.js
@@ -2,7 +2,15 @@ const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+app.use(function (req, res, next) {
+	express.json();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 const apiRouter = require("./routes/api");
 app.use("/api", apiRouter);
--- grid-dashboard/src/DatabaseInterface.js
+++ grid-dashboard/src/DatabaseInterface.js
@@ -2,21 +2,21 @@
 // It can be imported into any file that needs to use this information, decreasing code duplication
 
 // Define the base URI to make it easier to change
-const base_URI = 'https://www.geysertimes.org/api/v5';
+const base_URI = "https://www.geysertimes.org/api/v5";
 
 // A function that async queryies the database for some information
 // URI_extension is a string represnting the rest of the path
 export async function getGeyserInfo(URI_extension, list) {
-  // Get the json using the database's REST api
-  const response = await fetch(base_URI + URI_extension);
-  const my_json = await response.json();
+	// Get the json using the database's REST api
+	const response = await fetch(base_URI + URI_extension);
+	const my_json = await response.json();
 
-  // Get a specific part of the data
-  const geysers = await my_json.geysers;
-  // console.log(geysers[0]);
+	// Get a specific part of the data
+	const geysers = await my_json.geysers;
+	// console.log(geysers[0]);
 
-  // Populate the target list such that AsyncSelect can use it
-  for(let i = 0; i < 10; i++) {
-    list.push({value: geysers[i].id, label: geysers[i].name});
-  }
+	// Populate the target list such that AsyncSelect can use it
+	for (let i = 0; i < 10; i++) {
+		list.push({ value: geysers[i].id, label: geysers[i].name });
+	}
 }
